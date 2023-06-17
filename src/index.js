8ceb01db951f264349cd9074d024d7f288febf10
Nakami Hope-Felix <hopefelixn@gmail.com>
2023-06-16 20:01:42 -0400

    Fixed package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "/client";
+import ReactDOM from "react-dom/client";
 
 import "assets/css/nucleo-icons.css";
 import "assets/scss/blk-design-system-react.scss";
